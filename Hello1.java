b790fe2ec756f4b5084c2b3677ab5deb06a03091
darshan838 <70249886+darshan838@users.noreply.github.com>
2021-12-07 21:56:57 +0530

    Update Hello1.java

--- Hello1.java
+++ Hello1.java
@@ -4,5 +4,6 @@ public static void main(String[] args) {
 	System.out.println("Hello World");
 	System.out.println("I have just Integrated Github with Jenkins");
 	System.out.println("Continueing working on GitHub and Jenkins and it is easy also");
+	System.out.println("This is for Pull to Loxal Repository");
 }
 }
